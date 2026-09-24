e11871dc26436c29d9f531ed2a677709697e3176
Aerhead09 <Jeankisekiwaluyo09@gmail.com>
2026-09-24 19:17:34 +0700

    Nambah beberapa cycle baru

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -51,19 +51,24 @@ void Chip8::loadROM(const std::string& filename) {
 }
 
 void Chip8::cycle() {
-    std::cout << "PC: 0x" << std::hex << pc
-          << "  Opcode: 0x" << opcode
-          << std::dec
-          << "  VA: " << (int)V[0xA]
-          << "  VB: " << (int)V[0xB]
-          << std::endl;
     if (pc + 1 >= MEMORY_SIZE) {
-        std::cerr << "PC keluar batas: 0x" << std::hex << pc << std::dec << "\n";
+        static bool warned = false;
+        if (!warned) {
+            std::cerr << "PC keluar batas: 0x" << std::hex << pc << std::dec << "\n";
+            warned = true;
+        }
         return;
     }
 
     opcode = (memory[pc] << 8) | memory[pc + 1];
     
+    //std::cout << "PC: 0x" << std::hex << pc
+    //      << "  Opcode: 0x" << opcode
+    //      << std::dec
+    //      << "  VA: " << (int)V[0xA]
+    //      << "  VB: " << (int)V[0xB]
+    //      << std::endl;
+
     pc += 2;
 
     switch (opcode & 0xF000) {
@@ -75,6 +80,11 @@ void Chip8::cycle() {
             }
             break;
         case 0x1000:
+            pc = opcode & 0x0FFF;
+            break;
+        case 0x2000:
+            stack[sp] = pc;
+            sp++;
             pc = opcode & 0x0FFF;
             break;
         case 0x6000: {
@@ -92,5 +102,41 @@ void Chip8::cycle() {
         case 0xA000:
             I = opcode & 0x0FFF;
             break;
+        case 0xD000: {
+            uint8_t X = (opcode & 0x0F00) >> 8;
+            uint8_t Y = (opcode & 0x00F0) >> 4;
+            uint8_t N = opcode & 0x000F;
+
+            uint8_t x = V[X] % VIDEO_WIDTH;
+            uint8_t y = V[Y] % VIDEO_HEIGHT;
+
+            V[0xF] = 0;
+
+            for (int row = 0; row < N; row++) {
+                uint8_t spriteByte = memory[I + row];
+                for (int col = 0; col < 8; col++) {
+                    if ((spriteByte & (0x80 >> col)) != 0) {
+                        int px = (x + col) % VIDEO_WIDTH;
+                        int py = (y + row) % VIDEO_HEIGHT;
+                        int idx = py * VIDEO_WIDTH + px;
+
+                        if (video[idx] == 1) {
+                            V[0xF] = 1;
+                        }
+                        video[idx] ^= 1;
+                    }
+                }
+            }
+            break;
+        }
+        case 0xF000: {
+            uint8_t X = (opcode & 0x0F00) >> 8;
+            switch (opcode & 0x00FF) {
+                case 0x29: 
+                    I = V[X] * 5;
+                    break;
+            }
+            break;
+        }
     }
 }
